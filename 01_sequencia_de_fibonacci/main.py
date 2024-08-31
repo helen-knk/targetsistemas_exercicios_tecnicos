2d858938375cc82e3e8e546985fc6c3b4e8b2a5a
Helen Christine <helen.christine@mercos.com>
2024-08-30 22:52:18 -0300

    Adiciona resposta do exercício 01

---
+++ 01_sequencia_de_fibonacci/main.py
@@ -0,0 +1,23 @@
+numero = int(input('Digite um número: '))
+
+def numero_pertence_a_sequencia_de_fibonacci(numero):
+    primeiro_numero = 0
+    segundo_numero = 1
+    numero_da_sequencia = 0
+    sequencia_fibonacci = []
+    def calcula_sequencia_de_fibonacci(numero_da_sequencia, primeiro_numero, segundo_numero):
+        while numero_da_sequencia < numero:
+            numero_da_sequencia = primeiro_numero + segundo_numero
+            primeiro_numero = segundo_numero
+            segundo_numero = numero_da_sequencia
+            sequencia_fibonacci.append(numero_da_sequencia)
+    calcula_sequencia_de_fibonacci(numero_da_sequencia, primeiro_numero, segundo_numero)
+
+    def verifica_se_numero_digitado_esta_na_lista(numero, sequencia_fibonacci):
+        if numero in sequencia_fibonacci:
+            print(f"O número {numero} pertence a sequência de Fibonacci")
+        else:
+            print(f"O número {numero} não pertence a sequência de Fibonacci")
+    verifica_se_numero_digitado_esta_na_lista(numero, sequencia_fibonacci)
+
+numero_pertence_a_sequencia_de_fibonacci(numero)
